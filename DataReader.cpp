6b4e8876450b4ce3c579a249b35ae5b9f724912f
Stamaks <A6437149@yandex.ru>
2019-04-10 01:26:49 +0300

    Smth done

--- DataReader.cpp
+++ DataReader.cpp
@@ -26,24 +26,39 @@ public:
         std::cout << "Начинаю тест алгоритма " << alg_name << " на " << number_of_vertexes << " вершин.\n";
     }
 
-    void read(std::vector<std::vector<int>>& flows)
+    void read(std::vector<std::vector<int>>& flows, int& source, int& sink)
     {
         if (!fin.is_open())
             throw std::ios_base::failure("file not open");
 
+        std::vector<int> not_sink(number_of_vertexes);
+
         std::string s;
         int cur;
+        bool is_source;
         for (int i = 0; i < number_of_vertexes; ++i)
         {
             std::vector<int>* vec = new std::vector<int>();
 
+            is_source = true;
             for (int j = 0; j < number_of_vertexes; ++j)
             {
                 fin >> cur;
+                if (cur != 0) {
+                    is_source = false;
+                    not_sink[j] = 1;
+                }
                 vec->push_back(cur);
             }
 
+            if (is_source)
+                source = i;
+
             flows.push_back(*vec);
         }
+
+        for (int i = 0; i < not_sink.size(); ++i)
+            if (!not_sink[i])
+                sink = i;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 #include "FlowNetwork.cpp"
 #include "DataReader.cpp"
 
 using namespace std;
 
-int dft(int u, int Cmin)
+int dft(int current, int flow, vector<vector<int>>& flows, vector<vector<int>>& cur_flows,
+        vector<bool>& visited, const int& source, const int& sink)
 {
+    if (current == sink)
+        return flow;
+
+    visited[current] = true;
+    for (int i = 0; i < flows[current].size(); ++i)
+    {
+        if (!flows[current][i])
+            continue;
+
+        if (!visited[i] and cur_flows[current][i] < flows[current][i])
+        {
+            int delta = dft(i, min(flow, flows[current][i] - cur_flows[current][i]));
+            if (delta > 0)
+            {
+                cur_flows[current][i] += delta;
+                cur_flows[i][current] -= delta;
+                return delta;
+            }
+        }
+    }
 
 }
 
 int main() {
     DataReader* dr = new DataReader("../../kdzdata/input_10_0.0.txt");
 
-    vector<vector<int>> v;
-    dr->read(v);
+    vector<vector<int>> flows;
+    int source, sink;
+    dr->read(flows, source, sink);
 
-    for (int i = 0; i < v.size(); ++i)
-    {
-        for (int j = 0; j < v[i].size(); ++j)
-        {
-            cout << v[i][j] << " ";
-        }
+    cout << source << sink;
 
-        cout << "\n";
-    }
+    vector<vector<int>> current_flows(flows.size(), vector<int>(flows.size()));
+
+    double start_time = clock();
+
+//    dft();
+
+    double end_time = clock();
+    double res_time = ((end_time - start_time) / CLOCKS_PER_SEC);
+
 }
